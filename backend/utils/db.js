f322d52babded37a8d5c2acc8ad8e8117fd14dbc
Abdullah <yousaf.8666305@gmail.com>
2025-03-15 06:17:26 +0500

    change in mongodburi

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
 	try {
-		const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/instagram`);
+		const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/instagram`, { useNewUrlParser: true, useUnifiedTopology: true });
 		console.log(`MongoDB connected !! DB HOST; ${connectionInstance.connection.host}`)
 	} catch (error) {
 		console.log("DB connection FAILD: ", error);
