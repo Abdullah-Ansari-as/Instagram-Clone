7dd363856f34fd94b50cdfd7ed0b324e04f6b1ad
Abdullah <yousaf.8666305@gmail.com>
2025-03-15 06:25:57 +0500

    problem

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
 	try {
-		const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/instagram`, { useNewUrlParser: true, useUnifiedTopology: true });
+		console.log(process.env.MONGODB_URI)
+		const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 		console.log(`MongoDB connected !! DB HOST; ${connectionInstance.connection.host}`)
 	} catch (error) {
 		console.log("DB connection FAILD: ", error);
